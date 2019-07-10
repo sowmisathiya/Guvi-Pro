685ed0c01e5e20fac5bcaddf13e72c3a4e2df582
SANGEETHA DEVENDRAN <51980967+SangeethaDevendran@users.noreply.github.com>
2019-07-10 17:12:45 +0530

    Create s24.py

---
+++ s24.py
@@ -0,0 +1,13 @@
+cha,che=map(int,input().split())
+law=list(map(int,input().split()))
+cha=[]
+law.insert(0,0)
+for p in range(che):
+     vim=[]
+     s=0
+     but,zee=map(int,input().split())
+     for i in range(but,zee+1):         
+         sha^=law[i]     
+     cha.append(sha)
+for p in cha:
+     print(p)

4dee45c94b84765991315fc19df051ca4b25e1f8
SANGEETHA DEVENDRAN <51980967+SangeethaDevendran@users.noreply.github.com>
2019-07-10 17:27:05 +0530

    Update s27.py

--- s27.py
+++ s27.py
@@ -3,7 +3,7 @@ law=list(map(int,input().split()))
 cat=0
 for g in range(len(law)):
   for h in range(len(law)):
-    if law[g]+l[h]==que:
+    if law[g]+law[h]==que:
       cat+=1
 if(cat>0):
   print("yes")

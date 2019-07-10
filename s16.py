dfad20b7f85a3c6d49f005f04c9f03284f6c2314
SANGEETHA DEVENDRAN <51980967+SangeethaDevendran@users.noreply.github.com>
2019-07-10 16:15:24 +0530

    Update s16.py

--- s16.py
+++ s16.py
@@ -4,6 +4,6 @@ h=0
 for g in range(len(sha)-2):
     for p in range(g+1,len(sha)-1):
         for love in range(p+1,len(sha)):
-            if sha[g]<sha[i]<sha[love] and g<p<love:
+            if sha[g]<sha[p]<sha[love] and g<p<love:
                 h=h+1
 print(h)

7bbc4f436f28d30f0b1d03f5ea9b0c1f4086c123
SANGEETHA DEVENDRAN <51980967+SangeethaDevendran@users.noreply.github.com>
2019-07-10 16:10:58 +0530

    Create s16.py

---
+++ s16.py
@@ -0,0 +1,9 @@
+cha=int(input())
+sha=list(map(int,input().split()))
+h=0
+for g in range(len(sha)-2):
+    for p in range(g+1,len(sha)-1):
+        for love in range(p+1,len(sha)):
+            if sha[g]<sha[i]<sha[love] and g<p<love:
+                h=h+1
+print(h)

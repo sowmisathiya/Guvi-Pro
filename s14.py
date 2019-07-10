cba8ffcf76a9d029fdaa474ba41c987d2b4501e2
SANGEETHA DEVENDRAN <51980967+SangeethaDevendran@users.noreply.github.com>
2019-07-10 15:52:47 +0530

    Create s14.py

---
+++ s14.py
@@ -0,0 +1,12 @@
+    
+cha,cho=map(str,input().split())
+yas=0
+if len(cha)>len(cho):
+  cha,cho=cho,cha
+p=0
+while p<len(cha):
+  yas+=(ord(cho[p])-ord(cha[p]))
+  p+=1
+for p in range(p,len(cho)):
+  yas+=ord(cho[p])-ord('a')+1
+print(yas)

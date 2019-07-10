cd6a3cbc6611acac4c6b31fc190e6411688d572f
SANGEETHA DEVENDRAN <51980967+SangeethaDevendran@users.noreply.github.com>
2019-07-10 16:57:52 +0530

    Rename s21.py to s22.py

---
+++ s22.py
@@ -0,0 +1,11 @@
+cha,saw=map(int,input().split())
+lost=list(map(int,input().split()))
+law=[]
+for p in range(0,saw):
+     sha,sha=map(int,input().split())
+     law.append([sha,she])
+for p in range(saw):
+     las=law[p][0]
+     upo=law[p][1]
+     yaw=sum(lost[las-1:upo])
+     print(yaw)

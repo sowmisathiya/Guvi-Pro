a01b1d4725c24ff9f17db30e540fb4674735877a
SANGEETHA DEVENDRAN <51980967+SangeethaDevendran@users.noreply.github.com>
2019-07-16 17:21:25 +0530

    Create s37.py

---
+++ s37.py
@@ -0,0 +1,17 @@
+sha,kaa=map(int,input().split())
+psa=list(map(int,input().split()))
+vsa=list(map(int,input().split()))
+tsa=[]
+cina=0
+for i in range(sha):
+    xaa=vsa[i]/psa[i]
+    tsa.append(x)
+while kaa>=0 and len(tsa)>0:
+    mindexa=tsa.index(max(tsa))
+    if kaa>=psa[mindexa]:
+        cina=cina+vsa[mindexa]
+        kaa=kaa-psa[mindexa]
+    psa.pop(mindexa)
+    vsa.pop(mindexa)
+    tsa.pop(mindexa)
+print(cina)

9fe0fd66a4da881b7da81ad3a54c502373b3a4ee
SANGEETHA DEVENDRAN <51980967+SangeethaDevendran@users.noreply.github.com>
2019-07-25 22:00:03 +0530

    Update s410.py

--- s410.py
+++ s410.py
@@ -1,8 +1,8 @@
 cha=input()
 che=0
 for i in range(0,len(cha)):
-    sha=(cha[:i]+cha[i+1:])
-    if(int(sha)%8==0):
+    sha=(cha[i:i]+cha[i+1:])
+    if(len(sha)%8==0):
         che=1
         break
 if(che==1):

f8024e9caf2dc3436f73ce398fb55be05c4cd05a
SANGEETHA DEVENDRAN <51980967+SangeethaDevendran@users.noreply.github.com>
2019-07-25 19:48:32 +0530

    Create s51.py

---
+++ s51.py
@@ -0,0 +1,26 @@
+cho,cha=map(int,input().split())
+sha=''
+che=2
+if(cho+cha<=3):
+    for i in range(0,cho+cha):
+        if(i%2!=0):
+            sha=sha+'0'
+        else:
+            sha=sha+'1'
+else:    
+    for i in range(0,cho+cha):
+        if(i==urn):
+            sha=sha+'0'
+            if(che==cha):
+                che=che+2
+            else:
+                che=che+3
+        else:
+            sha=sha+'1'
+sho=len(sha)-1
+if(int(sha[sho])==0):
+    print('-1') 
+elif cho==1 and cha==2: 
+     print("011")
+else:
+    print(sha)

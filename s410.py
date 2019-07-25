6c1ad908b2b9b869eb94d37f41871dcaae9ea637
SANGEETHA DEVENDRAN <51980967+SangeethaDevendran@users.noreply.github.com>
2019-07-25 21:34:35 +0530

    Create s410.py

---
+++ s410.py
@@ -0,0 +1,11 @@
+cha=input()
+che=0
+for i in range(0,len(cha)):
+    sha=(cha[:i]+cha[i+1:])
+    if(int(sha)%8==0):
+        che=1
+        break
+if(che==1):
+    print("yes")
+else:
+    print("no")

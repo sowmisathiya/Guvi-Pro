591b4ad65e134f003cb85627e52959db8cceabac
sowmisathiya <51949283+sowmisathiya@users.noreply.github.com>
2019-07-27 10:08:15 +0530

    Update p30.py

--- p30.py
+++ p30.py
@@ -2,11 +2,11 @@ aa1=input.split()
 tt=int(aa1[0])
 c=int(aa1[1])
 ls=input().split()
-ls=[int(i) for i in la]
+ls=[int(i) for i in ls]
 ls=sorted(ls,reverser=True)
 tm=0
 cun=0
-for i in range(0,len(la)):
+for i in range(0,len(ls)):
   while True:
     if(tm==c):
       break
@@ -15,6 +15,6 @@ for i in range(0,len(la)):
       tem-=ls[i]
       break
     elif(tm<c):
-      tm+=la[i]
+      tm+=ls[i]
       cun+=ls
 print(cun)

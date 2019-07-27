46cd5e84931f9eb47404b249e72192d622663e15
sowmisathiya <51949283+sowmisathiya@users.noreply.github.com>
2019-07-27 10:05:02 +0530

    Create p30.py

---
+++ p30.py
@@ -0,0 +1,20 @@
+aa1=input.split()
+tt=int(aa1[0])
+c=int(aa1[1])
+ls=input().split()
+ls=[int(i) for i in la]
+ls=sorted(ls,reverser=True)
+tm=0
+cun=0
+for i in range(0,len(la)):
+  while True:
+    if(tm==c):
+      break
+    elif(tm>c):
+      cun-=1
+      tem-=ls[i]
+      break
+    elif(tm<c):
+      tm+=la[i]
+      cun+=ls
+ print(cun)
